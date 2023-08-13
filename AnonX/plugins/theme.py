b872374cd3e359e64bf1e8ab0b47acfd8c8b0ec5
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2023-08-13 11:35:26 +0300

    Update theme.py

--- AnonX/plugins/theme.py
+++ AnonX/plugins/theme.py
@@ -4,8 +4,8 @@ from pyrogram import Client, filters
 
 from config import BOT_USERNAME, MUSIC_BOT_NAME
 from AnonX import app
-from AnonX.utils.Database import (_get_theme, get_theme, save_theme)
-from AnonX.utils.Decorators.permission import PermissionCheck
+from AnonX.utils.database import (_get_theme, get_theme, save_theme)
+from AnonX.utils.decorators.permission import PermissionCheck
 
 themes = [
     "blue",
